5198bf51fbef4257b8b3dfce5653567b861fb3ec
Dana Prakoso <danaossoftware@gmail.com>
2019-02-25 22:13:04 +0700

    commit

---
+++ php/set-user-location.php
@@ -0,0 +1,8 @@
+<?php
+include 'db.php';
+$lat = doubleval($_POST["lat"]);
+$lng = doubleval($_POST["lng"]);
+session_id("kongsicargo");
+session_start();
+$userId = $_SESSION["kongsicargo_user_id"];
+$c->query("UPDATE drivers SET latitude=$lat, longitude=$lng WHERE id='$userId'");
