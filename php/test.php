409c3cb8c2b496b790d6b2dee4349320a28d6df7
Dana Prakoso <danaossoftware@gmail.com>
2019-02-25 16:07:21 +0700

    commit

--- php/test.php
+++ php/test.php
@@ -1,3 +1,3 @@
 <?php
-include 'db.php';
-$c->query("INSERT INTO drivers (id, email, password, phone, name) VALUES ('" . uniqid() . "', 'danaossoftware@gmail.com', 'Hello123', '081123456789', 'Driver 1')");
+include 'mail.php';
+sendMail("support@kongsikongsi.com", "danaossoftware@gmail.com", "This is subject", "Test");
