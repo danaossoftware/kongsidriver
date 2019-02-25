fefe3326cb1e067f71b4b240a6bd436fb9d6549b
Dana Prakoso <danaossoftware@gmail.com>
2019-02-25 21:38:44 +0700

    commit

--- php/send-message.php
+++ php/send-message.php
@@ -4,7 +4,6 @@ $messageId = uniqid();
 $senderId = $_POST["sender_id"];
 $receiverId = $_POST["receiver_id"];
 $message = $_POST["message"];
-$title = $_POST["title"];
 $date = round(microtime(true)*1000);
-$c->query("INSERT INTO messages (id, sender_id, receiver_id, title, message, date) VALUES ('" . $messageId . "', '" . $senderId . "', '" . $receiverId . "', '" . $title . "', '" . $message . "', " . $date . ")");
+$c->query("INSERT INTO messages (id, sender_id, receiver_id, message, date) VALUES ('" . $messageId . "', '" . $senderId . "', '" . $receiverId . "', '" . $message . "', " . $date . ")");
 echo $messageId;
