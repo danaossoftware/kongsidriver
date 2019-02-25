495ca5ae9238e011d49bbea9ea124feadeaaaefd
Dana Prakoso <danaossoftware@gmail.com>
2019-02-25 16:08:31 +0700

    commit

--- php/send-confirmation-email.php
+++ php/send-confirmation-email.php
@@ -3,5 +3,5 @@ include 'mail.php';
 $email = $_POST["email"];
 $confirmCode = uniqid();
 $confirmCode = substr($confirmCode, strlen($confirmCode)-6, 6);
-sendMail("support@kongsikongsi.com", $email, "Konfirmasi pendaftaran Kongsi Driver", "Mohon selesaikan pendaftaran dengan memasukkan kode berikut di layar yang tersedia:<br/><span style='color: #55efc4; font-size: 25sp; font-weight: bold;'>" . $confirmCode . "</span>");
+sendMail("support@kongsikongsi.com", $email, "Konfirmasi pendaftaran Kongsi Driver", "Mohon selesaikan pendaftaran dengan memasukkan kode berikut di layar yang tersedia:<br/><span style='color: #55efc4; font-size: 30px; font-weight: bold;'>" . $confirmCode . "</span>");
 echo $confirmCode;
