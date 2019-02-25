96ab7e83bcf6d82ffebd927ea9e5fb93c4f3cccb
Dana Prakoso <danaossoftware@gmail.com>
2019-02-25 16:01:40 +0700

    commit

--- php/send-confirmation-email.php
+++ php/send-confirmation-email.php
@@ -2,6 +2,6 @@
 include 'mail.php';
 $email = $_POST["email"];
 $confirmCode = uniqid();
-$confirmCode = substr($confirmCode, 0, 4);
+$confirmCode = substr($confirmCode, strlen($confirmCode)-6, 6);
 sendMail("support@kongsikongsi.com", $email, "Konfirmasi pendaftaran Kongsi Driver", "Mohon selesaikan pendaftaran dengan memasukkan kode berikut di layar yang tersedia:<br/><span style='color: #55efc4; font-size: 25sp; font-weight: bold;'>" . $confirmCode . "</span>");
 echo $confirmCode;
--- php/signup.php
+++ php/signup.php
@@ -17,7 +17,7 @@ if ($results && $results->num_rows > 0) {
 	$userId = uniqid();
 	$c->query("INSERT INTO drivers (id, email, password, phone, name) VALUES ('" . $userId . "', '" . $email . "', '" . $password . "', '" . $phone . "', '" . $name . "')");
 	$confirmCode = uniqid();
-	$confirmCode = substr($confirmCode, strlen($confirmCode)-4, 4);
+	$confirmCode = substr($confirmCode, strlen($confirmCode)-6, 6);
 	sendMail("support@kongsikongsi.com", $email, "Konfirmasi pendaftaran Kongsi Driver", "Mohon selesaikan pendaftaran dengan memasukkan kode berikut di layar yang tersedia:<br/><span style='color: #55efc4; font-size: 25sp; font-weight: bold;'>" . $confirmCode . "</span>");
 	echo $confirmCode;
 }
