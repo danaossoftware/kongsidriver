8dba8fb55f6335141ceb9c6ee37dd2ef027be4b8
Dana Prakoso <danaossoftware@gmail.com>
2019-02-25 22:19:59 +0700

    commit

--- php/set-driver-location.php
+++ php/set-driver-location.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
-$lat = doubleval($_GET["lat"]);
-$lng = doubleval($_GET["lng"]);
+$lat = doubleval($_POST["lat"]);
+$lng = doubleval($_POST["lng"]);
 session_id("kongsicargo");
 session_start();
 $userId = $_SESSION["kongsicargo_user_id"];
