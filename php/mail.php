7af110a2082c5dc96a8efc7f4143fa62dda41ccb
Dana Prakoso <danaossoftware@gmail.com>
2019-02-25 16:00:56 +0700

    commit

--- php/mail.php
+++ php/mail.php
@@ -1,11 +1,11 @@
 <?php
 
-function sendMail($src, $dst, $subject, $mail) {
+function sendMail($src, $dst, $subject, $msg) {
 	$to      = $dst;
-	$message = $mail;
-	$headers = 'From: ' . $src . '\r\n' .
-    'Reply-To: ' . $dst . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-	$headers .= "Content-type: text/html; charset=iso-8859-1rn";
+	$message = $msg;
+	$headers = 'From: ' . $src . "\r\n" .
+    'Reply-To: ' . $src . "\r\n" .
+    'X-Mailer: PHP/' . phpversion() . "\r\n" .
+	"Content-type: text/html; charset=iso-8859-1rn";
 	mail($to, $subject, $message, $headers);
 }
