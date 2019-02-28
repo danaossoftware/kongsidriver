0b2ff6c7209816fd9d8e848a150ace446e32ea2d
Dana Prakoso <danaossoftware@gmail.com>
2019-03-01 02:21:29 +0700

    commit

--- php/get-total-orders.php
+++ php/get-total-orders.php
@@ -4,7 +4,7 @@ session_id("kongsidriver");
 session_start();
 $userId = $_SESSION["kongsidriver_user_id"];
 $results = $c->query("SELECT * FROM orders WHERE driver_id='" . $userId . "'");
-if ($results && $results->num_rows > 0) {
+if ($results) {
 	echo $results->num_rows;
 } else {
 	echo 0;
