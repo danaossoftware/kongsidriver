c3be8874ae3f9c1faacb82081d28dc5eb0bf73f3
Dana Prakoso <danaossoftware@gmail.com>
2019-03-02 14:08:46 +0700

    commit

---
+++ php/get-completed-orders.php
@@ -0,0 +1,13 @@
+<?php
+include 'db.php';
+session_id("kongsidriver");
+session_start();
+$driverId = $_SESSION["kongsidriver_user_id"];
+$results = $c->query("SELECT * FROM orders WHERE driver_id='" . $driverId . "' AND status='done'");
+$orders = [];
+if ($results && $results->num_rows > 0) {
+	while ($row = $results->fetch_assoc()) {
+		array_push($orders, $row);
+	}
+}
+echo json_encode($orders);
